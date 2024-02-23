ecf00653878e1a4226873299d98265eedde0e151
zakheledyosba <zakheledyosiba94@gmail.com>
2024-02-23 10:13:14 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,6 +27,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, l;
 
+	if (array == NULL || size == 0)
+	{
+		printf("Array is empty");
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
